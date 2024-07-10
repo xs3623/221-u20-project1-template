a524da7e391c1e86e3194c4b2395c7089aae2bf6
Xzavier Suliveres <xs3623@students.necsd.net>
2024-07-10 11:02:13 -0400

    google

--- server/appServer.js
+++ server/appServer.js
@@ -6,6 +6,8 @@ app.get('/', function(req, res) {
     res.sendFile('index.html', {root: './client/views'})
 })
 
-
+app.get('/feed', function(req, res) {
+    res.sendFile('feed.html', {root: './client/views'})
+})
 
 app.listen(1337, () => console.log('Listening on port 1337.'))
